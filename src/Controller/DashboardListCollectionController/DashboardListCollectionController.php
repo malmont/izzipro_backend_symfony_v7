31b586708c6ba81999a8340bf44260c7aea25622
Almont michel <michel.almont@gmail.com>
2024-08-22 16:39:58 +0000

    DashboaardController

---
+++ src/Controller/DashboardListCollectionController/DashboardListCollectionController.php
@@ -0,0 +1,100 @@
+<?php
+namespace App\Controller\DashboardListCollectionController;
+
+use App\Entity\Collections;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardListCollectionController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/api/dashboard/collection/{id}', name: 'dashboard_collection', methods: ['GET'])]
+    public function dashboardCollection(Collections $collection): JsonResponse
+    {
+        $commandes = $collection->getCommandes();
+    
+        $totalItemCost = 0;
+        $totalShippingCost = 0;
+        $totalExpenseCost = 0;
+        $totalQuantity = 0;
+        $totalMultiplier = 0;
+        $itemCount = 0;
+        $countProduct=0;
+        $stockValue=0;
+    
+        foreach ($commandes as $commande) {
+            $products = $commande->getProducts();
+            $countProduct+=count($products);
+            foreach ($products as $product) {
+                $totalItemCost += $product->getPrice() * $product->getQuantity();
+                $totalQuantity += $product->getQuantity();
+                $totalMultiplier += $product->getCoefficientMultiplier() * $product->getQuantity();
+                $stockValue+=$product->getPrice() * $product->getQuantity()*$product->getCoefficientMultiplier();
+                $itemCount += $product->getQuantity();
+            }
+            if ($commande->getFraisDePort()) {
+                $totalShippingCost += $commande->getFraisDePort()->getPrice();
+            }
+        }
+        
+        $averageMultiplier = $totalQuantity > 0 ? $totalMultiplier / $totalQuantity : 0;
+        
+        foreach ($collection->getNoteDeFrais() as $note) {
+            $totalExpenseCost += $note->getMontant();
+        }
+    
+        $generalBudget = $collection->getBudgetCollection();
+        $usedBudget = $totalItemCost + $totalShippingCost + $totalExpenseCost;
+        $remainingBudget = $generalBudget - $usedBudget;
+    
+        $startDate = $collection->getStartDateCollection();
+        $endDate = $collection->getEndDateCollection();
+        $durationDays = $startDate->diff($endDate)->days;
+    
+       
+    
+        $response = [
+            'averageMultiplier' => $averageMultiplier,
+            'BudgetGeneral' => [
+            'generalBudget' => $generalBudget,
+            'usedBudget' => $usedBudget,
+            'remainingBudget' => $remainingBudget,
+            ],
+            'CollectionDuration' => [
+                'startDate' => $startDate->format('Y-m-d'),
+                'endDate' => $endDate->format('Y-m-d'),
+                'days' => $durationDays,
+            ],
+            'totalItemCost' => $totalItemCost,
+            'GeneralExpenses' => [
+                'totalShippingCost' => $totalShippingCost,
+                'totalExpenseCost' => $totalExpenseCost,
+                'totalGeneralExpenses' => ($totalShippingCost + $totalExpenseCost),
+            ],
+            'Statistics' => [
+                'orderCount' => count($commandes),
+                'itemCount' => $itemCount,
+                'modelCount' => $countProduct, // Assuming each product is a model
+            ],
+            'ValeurStock' => [
+            'stockValue' => $stockValue,
+            'marge'=>($stockValue - $usedBudget),
+            ],
+            'TauxMarge' => [
+                'tauxMarge' => (($stockValue - $usedBudget)/$stockValue)*100,
+                'tauxMarque'=>(($stockValue - $usedBudget)/$usedBudget)*100,
+            ]
+        ];
+    
+        return $this->json($response);
+    }
+    
+}
