1f0bcee8b944a0bb2b9003a64e36fbbeb4b11c6e
Almont michel <michel.almont@gmail.com>
2025-01-11 23:06:06 +0000

    add new migration and cookie_samesite: strict

--- migrations/Version20250111191417.php
+++ migrations/Version20250111191417.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241025175921 extends AbstractMigration
+final class Version20250111191417 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,12 @@ final class Version20241025175921 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "order" ADD status_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE "order" DROP status_updated_at');
     }
 }
