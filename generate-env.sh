703bdcb349b95978b443cab2d7f102bb194c3a26
Almont michel <michel.almont@gmail.com>
2025-01-15 22:44:34 +0000

    nouveau test

--- generate-env.sh
+++ generate-env.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-APP_DIR="."  # Dossier actuel
+echo "📄 Création sécurisée du fichier .env..."
 
-if [ ! -f "$APP_DIR/.env" ]; then
-  echo "📄 Création sécurisée du fichier .env..."
-
-  cat <<EOL > "$APP_DIR/.env"
+cat <<EOL > .env
 POSTGRES_USER=${DB_USER}
 POSTGRES_PASSWORD=${DB_PASSWORD}
 POSTGRES_DB=${DB_NAME}
 POSTGRES_HOST=${DB_HOST}
 POSTGRES_PORT=${DB_PORT}
-APP_ENV=${APP_ENV}
+APP_ENV=prod
 APP_SECRET=${APP_SECRET}
 DATABASE_URL=pgsql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 EOL
 
-  echo "✅ Fichier .env généré dans $APP_DIR."
-else
-  echo "ℹ️ Le fichier .env existe déjà dans $APP_DIR."
-fi
+echo "✅ Fichier .env généré."
