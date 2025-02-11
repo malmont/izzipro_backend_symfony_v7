6889a677a12ba719ddf5da851d884ee29ea10dd7
Almont michel <michel.almont@gmail.com>
2025-02-11 01:12:15 +0000

    login actif si isverified

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isVerified()) {
+            // Vous pouvez personnaliser le message d'erreur
+            throw new CustomUserMessageAccountStatusException('Your account is not verified. Please check your email.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        // Vous pouvez effectuer d'autres vérifications après l'authentification si besoin
+    }
+}
