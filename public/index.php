69198746412a6d392c20ea2014c02b2cd4584861
Almont michel <michel.almont@gmail.com>
2025-04-09 19:40:36 +0000

    new config

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+if ('prod' === $_SERVER['APP_ENV']) {
+    Request::setTrustedProxies(
+        ['172.17.0.0/16'],
+        Request::HEADER_X_FORWARDED_ALL
+    );
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
