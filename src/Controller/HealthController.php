fa76f31282714f0cb86d409c5db105c80e999edf
Michel Almont <malmont@v2v.ca>
2026-05-12 16:28:28 +0000

    add end point /api/health

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/api/health', name: 'health_check', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+            'timestamp' => (new \DateTime())->format(\DateTimeInterface::ATOM),
+        ], JsonResponse::HTTP_OK);
+    }
+}
