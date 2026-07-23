0ffb3cfdfb9f671eb90b577ff14d763d10b08017
Michel Almont <malmont@v2v.ca>
2026-07-23 17:17:14 +0000

    feat(multitenant): map tenants to dedicated v7 databases v7_larameemarine, v7_caravane201, v7_expertnautique

--- src/Services/TenantConnectionProvider.php
+++ src/Services/TenantConnectionProvider.php
@@ -39,10 +39,18 @@ class TenantConnectionProvider
             $this->defaultConnection->close();
         }
 
+        // Target dedicated v7 database to keep GemSuite DBs untouched
+        $targetDb = match($tenantDbName) {
+            'db_larameemarineinc2', 'db_larameemarine' => 'v7_larameemarine',
+            'db_caravane201inc', 'db_caravane201' => 'v7_caravane201',
+            'db_expertnautique' => 'v7_expertnautique',
+            default => $tenantDbName,
+        };
+
         // Basculer dynamiquement le nom de la base de données de la connexion Doctrine par défaut
         $refParams = new ReflectionProperty(Connection::class, 'params');
         $params = $refParams->getValue($this->defaultConnection);
-        $params['dbname'] = $tenantDbName;
+        $params['dbname'] = $targetDb;
         $refParams->setValue($this->defaultConnection, $params);
 
         $refConn = new ReflectionProperty(Connection::class, '_conn');
